308e7364e621648f127722d00a4c7b99a29c5aec
Jonathan Goodwin <njo.goodwin+pro@gmail.com>
2024-09-05 09:54:30 -0700

    Initial commit for dbt transform and Soda checks

--- dags/retail.py
+++ dags/retail.py
@@ -8,6 +8,11 @@ from astro.files import File
 from astro.sql.table import Table, Metadata
 from astro.constants import FileType
 
+from include.dbt.cosmos_config import DBT_PROJECT_CONFIG, DBT_CONFIG
+from cosmos.airflow.task_group import DbtTaskGroup
+from cosmos.constants import LoadMode
+from cosmos.config import ProjectConfig, RenderConfig
+
 # Dag details for executing the pipeline
 @dag(
     start_date=datetime(2023, 1, 1),
@@ -60,6 +65,24 @@ def retail():
         return check(scan_name, checks_subpath)
     
     check_load()
+
+    transform = DbtTaskGroup(
+        group_id = 'transform',
+        project_config = DBT_PROJECT_CONFIG,
+        profile_config = DBT_CONFIG,
+        render_config = RenderConfig(
+            load_method = LoadMode.DBT_LS,
+            select = ['path:models/transform']
+        )
+    )
+
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_transform(scan_name='check_transform', checks_subpath='transform'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+    
+    check_transform()
 
 
 retail()
